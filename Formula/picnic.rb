86230d1c7ec1d3a40b2d67c8d3c4fde7b3121645
Benjamin Thorstesen <benithors@Benjamins-Mini.home>
2026-01-04 13:11:21 +0100

    Update picnic formula to v0.0.1

--- Formula/picnic.rb
+++ Formula/picnic.rb
@@ -1,6 +1,8 @@
 class Picnic < Formula
   desc "Tiny macOS menu bar app for fast screenshots"
   homepage "https://github.com/benithors/picnic"
+  url "https://github.com/benithors/picnic/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "ef68a0f812b61056149fc28351b87baff3998973013ede5a3a9e790c00b73f2a"
   head "https://github.com/benithors/picnic.git", branch: "main"
 
   depends_on :macos
@@ -12,10 +14,6 @@ class Picnic < Formula
   end
 
   test do
-    # Since it is a GUI app, testing it headless might be tricky if it doesn't have a help command that exits.
-    # We'll try running it and expecting it to fail or checking version if implemented.
-    # For now, just a placeholder test that usually works for swift apps if they parse args.
-    # If not, we might need to remove this or adapt.
-    system "#{bin}/picnic", "--help" 
+    system "#{bin}/picnic", "--help"
   end
 end
