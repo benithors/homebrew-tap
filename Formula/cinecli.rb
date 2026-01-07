88abab0b2923cd972ec3e8ae867bd9fe7a296078
benithors <thorstensen.benjamin@gmail.com>
2026-01-07 21:42:15 +0100

    Bump cinecli to v0.1.1 - Add upcoming movies feature

--- Formula/cinecli.rb
+++ Formula/cinecli.rb
@@ -1,8 +1,8 @@
 class Cinecli < Formula
   desc "CLI tool for browsing Cineplexx Austria cinemas, movies, and showtimes"
   homepage "https://github.com/benithors/cinecli"
-  url "https://github.com/benithors/cinecli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4f13441c52980cf835960b043dde2e61e6208114c64e9f94f7f2929da562f661"
+  url "https://github.com/benithors/cinecli/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "ec1a458374ad02614a187998bc542741056c36bec2e936f3117a421088084934"
   head "https://github.com/benithors/cinecli.git", branch: "main"
 
   depends_on "go" => :build
