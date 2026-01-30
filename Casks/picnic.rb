a40d767167e5683481508a38a23b25ad99845dcb
Benjamin Thorstesen <benjamin.thorstensen@kylo.at>
2026-01-30 14:49:11 +0100

    Update Picnic cask to 0.0.2

--- Casks/picnic.rb
+++ Casks/picnic.rb
@@ -1,6 +1,6 @@
 cask "picnic" do
-  version "0.0.1"
-  sha256 "af1cde314b8d5359ab90185b4cf648dfae9cbf4e1fc81683e302e30120745bcb"
+  version "0.0.2"
+  sha256 "53b0be2d0a9139831b985ce5c512349299b871c7f64903e5c54007d27949572c"
 
   url "https://github.com/benithors/picnic/releases/download/v#{version}/Picnic.app.zip"
   name "Picnic"
